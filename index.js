1efdc2dd22bd03d34e38b4e4962c830bfe0d546f
Ainur Rozikin <rozikinainur742@gmail.com>
2026-01-31 20:16:15 +0700

    menyelesaikan source code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const notifier = required('node-notifier');
-const moment = required('moment');
+
+const notifier = require('node-notifier');
+const moment = require('moment');
 const argTime = process.argv.slice[2];
 
 const POMODORO_DURATION = argTime[0];
@@ -7,4 +8,38 @@ const BREAK_DURATION = argTime[1];
 
 let isWorking = false;
 let reminingTime = 0;
+
+function formattingTime(totalSecond){
+    const duration = moment.duration('totalSecond', 'sedonds');
+    const hours = duration.hours().toString().padStart(2, '0');
+    const minutes = duration.minutes().toString().padStart(2, '0');
+    const seconds = duration.seconds().toString().padStart(2, '0');
+
+    return `${hours}:${minutes}:${seconds}`;
+}
+
+function startTimer(duration){
+    isWorking = !isWorking;
+    reminingTime = duration * 60;
+
+    const timer = setInterval(()=> {
+        reminingTime--;
+
+        const formattedTime = formattedTime(reminingTime);
+        console.log(`${isWorking ? "Work" : "Break"} : ${formattedTime}`);
+
+        if (reminingTime === 0) {
+            clearInterval(timer);
+            notifier.notify({
+                title : isWorking ? "Working Time" : "Break Timer",
+                message : isWorking ? "Good Work" : "Good Break!",
+                sound: true,
+                wait: true,
+            })
+        startTimer(isWorking ? BREAK_DURATION : POMODORO_DURATION);
+        }
+    }, 1000);
+}
+
+startTimer(POMODORO_DURATION);
 
